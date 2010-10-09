e6abe75741653bc22fc6ecf821dcd8c6f140441a
Tony Heupel <tonyheupel@gmail.com>
2010-10-09 07:09:25 -0700

    Actually add Options class that better mimics hashes and defaults of ruby's XmlSimple

---
+++ XmlSimple.NET/Options.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XmlSimple
+{
+    public class Options
+    {
+        public bool AttrPrefix { get; set; }
+        public bool NoAttr { get; set; }
+
+
+        public Options()
+        {
+            // Set the defaults for all options here
+            AttrPrefix = false;
+            NoAttr = false;
+        }
+    }
+}
