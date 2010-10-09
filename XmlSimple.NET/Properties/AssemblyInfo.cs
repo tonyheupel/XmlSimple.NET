2c5ca6985adcb940a8e9e6e7fb77496a3b2f8238
Tony Heupel <tonyheupel@gmail.com>
2010-10-09 07:12:54 -0700

    Update AssemblyInfo to show the extremely early nature of the project

--- XmlSimple.NET/Properties/AssemblyInfo.cs
+++ XmlSimple.NET/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("XmlSimple.NET")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# 4 dynamic implementation of something like XmlSimple from Ruby, XML::Simple from Perl, and SimpleXML from PHP")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Tony Heupel")]
 [assembly: AssemblyProduct("XmlSimple.NET")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © Tony Heupel 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
